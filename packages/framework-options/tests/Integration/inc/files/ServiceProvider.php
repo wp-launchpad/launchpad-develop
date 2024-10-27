8ab2453cacd95a4a68241ebd6c67c28a834ab3a2
COQUARD Cyrille <coquardcyr@gmail.com>
2024-10-27 14:31:49 +0100

    Added framework-options tests

--- packages/framework-options/tests/Integration/inc/files/ServiceProvider.php
+++ packages/framework-options/tests/Integration/inc/files/ServiceProvider.php
@@ -6,19 +6,11 @@ use LaunchpadCore\Container\AbstractServiceProvider;
 
 class ServiceProvider extends AbstractServiceProvider
 {
-
-    public function get_common_subscribers(): array
-    {
-        return [
-            Subscriber::class,
-        ];
-    }
-
     /**
      * @inheritDoc
      */
     protected function define()
     {
-        $this->register_service(Subscriber::class);
+        $this->register_common_subscriber(Subscriber::class);
     }
 }
